5da2af1b04bc7f74e90ad0f6ab5a2d135e097eb5
Naomi Yocum <naomiyocum24@gmail.com>
2022-08-30 17:22:51 -0600

    add fire_upon method and testing

--- lib/cell.rb
+++ lib/cell.rb
@@ -12,7 +12,21 @@ class Cell
       false
     end
   end
+
   def place_ship(type)
     @ship = type
   end
+
+  def fired_upon?
+    if (@ship.name == "Cruiser" && @ship.length < 3) || (@ship.name == "Submarine" && @ship.length < 2)
+      true
+    else
+      false
+    end
+  end
+
+  def fire_upon
+    @ship.hit
+  end
+
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -31,4 +31,15 @@ RSpec.describe Cell do
       expect(cell.empty?).to eq(false)
     end
   end
+  describe '#fire_upon' do
+    it 'fires ship' do
+      cell = Cell.new("B4")
+      cruiser = Ship.new("Cruiser", 3)
+      cell.place_ship(cruiser)
+      expect(cell.fired_upon?).to eq(false)
+      cell.fire_upon
+      expect(cell.ship.health).to eq(2)
+      expect(cell.fired_upon?).to eq(true)
+    end
+  end
 end
