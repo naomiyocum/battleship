9cb4e5ccb36aa2f6749ad3f60a96a76a66aa7407
Naomi Yocum <naomiyocum24@gmail.com>
2022-09-03 11:12:20 -0600

    feat: complete computer_setup method

--- lib/game.rb
+++ lib/game.rb
@@ -26,7 +26,7 @@ class Game
     @computer_board.valid_coors_cruiser
     @computer_board.place(@computer_cruiser, @computer_board.cruiser_coors.sample)
     @computer_board.valid_coors_submarine
-    
+
     poss_sample = @computer_board.submarine_coors.sample
     while @computer_board.valid_placement?(@computer_submarine, poss_sample) == false
       poss_sample = @computer_board.submarine_coors.sample
